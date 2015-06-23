2b3b95d77b36967b7ee6c204aa5cba99188022a4
Javis Sullivan <JavisSullivan@gmail.com>
2015-06-23 00:05:36 -0400

    continued

--- WeatherApp/main.py
+++ WeatherApp/main.py
@@ -1,3 +1,4 @@
+
 from __future__ import print_function
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout 
@@ -6,26 +7,44 @@ from kivy.properties import ObjectProperty, ListProperty, NumericProperty, Strin
 from kivy.network.urlrequest import UrlRequest 
 import json
 from kivy.uix.listview import ListItemButton
+from kivy.factory import Factory
 
 
 API_KEY = "ebdb10f67989131d05caadb1d21f7754"	# You should get your own
 
+def locations_args_converter( index, data_item):
+		city, country = data_item
+		return {'location' : (city, country)}
+
 class WeatherRoot(BoxLayout):
 	current_weather = ObjectProperty()
-	def show_current_weather(self, location=None):
-		
+	locations = ObjectProperty()
+
+	def show_current_weather(self, location=None):		
 		self.clear_widgets()
 		if self.current_weather is None:
 			self.current_weather = CurrentWeather()
+		if self.locations is None:
+			self.locations = Factory.Locations()
 		if location is not None:
 			self.current_weather.location = location
+			if location not in self.locations.locations_list.adapter.data:
+				self.locations.locations_list.adapter.data.append(location)
+				self.locations.locations_list._trigger_reset_populate()
+
 		self.current_weather.update_weather()	
 		self.add_widget(self.current_weather)
 		
 
 	def show_add_location_form(self):
 		self.clear_widgets()
-		self.add_widget(AddLocationForm())	
+		self.add_widget(AddLocationForm())
+
+		
+
+	def show_locations(self):
+		self.clear_widgets()
+		self.add_widget(self.locations)
 
 class LocationButton(ListItemButton):
 	location = ListProperty()
@@ -64,9 +83,9 @@ class AddLocationForm(BoxLayout):
 		self.search_results.adapter.data.extend(cities)
 		self.search_results._trigger_reset_populate()
 
-	def args_converter(self, index, data_item):
+	'''def args_converter(self, index, data_item):
 		city, country = data_item
-		return {'location' : (city, country)}
+		return {'location' : (city, country)}'''
 
 	def received_error(self, request, error):
 		print("Received an error: {}".format(error))
@@ -78,6 +97,7 @@ class AddLocationForm(BoxLayout):
 class CurrentWeather(BoxLayout):
 	location = ListProperty(['New York', 'US'])
 	conditions = StringProperty()
+	conditions_image = StringProperty()
 	temp = NumericProperty()
 	hi_temp = NumericProperty()
 	low_temp = NumericProperty()
@@ -90,6 +110,7 @@ class CurrentWeather(BoxLayout):
 	def weather_retrieved(self, request, data):
 		data = json.loads(data.decode()) if not isinstance(data, dict) else data
 		self.conditions = data['weather'][0]['description']
+		self.conditions_image = "http://openweathermap.org/img/w/{}.png".format(data['weather'][0]['icon'])
 		self.temp = data['main']['temp']
 		self.hi_temp = data['main']['temp_max']
 		self.low_temp = data['main']['temp_min']
