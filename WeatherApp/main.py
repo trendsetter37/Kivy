732527647323a9eec7a53435f591bb2d6c1bad7f
Javis Sullivan <JavisSullivan@gmail.com>
2015-06-25 23:15:34 -0400

    more work on app settings

--- WeatherApp/main.py
+++ WeatherApp/main.py
@@ -8,6 +8,7 @@ from kivy.network.urlrequest import UrlRequest
 import json
 from kivy.uix.listview import ListItemButton
 from kivy.factory import Factory
+from kivy.storage.jsonstore import JsonStore
 
 
 API_KEY = "ebdb10f67989131d05caadb1d21f7754"	# You should get your own
@@ -20,17 +21,31 @@ class WeatherRoot(BoxLayout):
 	current_weather = ObjectProperty()
 	locations = ObjectProperty()
 
+	def __init__(self, **kwargs):
+		super(WeatherRoot, self).__init__(**kwargs)
+		self.store = JsonStore("weather_store.json")
+		if self.store.exists("locations"):
+			current_location = self.store.get("locations")['current_location']
+			self.show_current_weather(current_location)
+
+
 	def show_current_weather(self, location=None):		
 		self.clear_widgets()
 		if self.current_weather is None:
 			self.current_weather = CurrentWeather()
 		if self.locations is None:
 			self.locations = Factory.Locations()
+			if self.store.exists('locations'):
+				locations = self.store.get("locations")['locations']
+				self.locations.locations_list.adapter.data.extend(locations)
 		if location is not None:
 			self.current_weather.location = location
 			if location not in self.locations.locations_list.adapter.data:
 				self.locations.locations_list.adapter.data.append(location)
 				self.locations.locations_list._trigger_reset_populate()
+				self.store.put("locations", 
+					locations=list(self.locations.locations_list.adapter.data),
+					current_location = location)
 
 		self.current_weather.update_weather()	
 		self.add_widget(self.current_weather)
@@ -101,10 +116,14 @@ class CurrentWeather(BoxLayout):
 	temp = NumericProperty()
 	hi_temp = NumericProperty()
 	low_temp = NumericProperty()
+
 	def update_weather(self):
+		config = WeatherApp.get_running_app().config
+		temp_type = config.getdefault("General", "temp_type", "metric").lower()
+
 		weather_template = "http://api.openweathermap.org/data/2.5/"+\
-		"weather?q={},{}&units=metric&APPID=" + API_KEY
-		weather_url = weather_template.format(*self.location) # Unpacking tuple / list with *
+		"weather?q={},{}&units={}&APPID=" + API_KEY
+		weather_url = weather_template.format(self.location[0], self.location[1], temp_type) # Unpacking tuple / list with *
 		request = UrlRequest(weather_url, self.weather_retrieved)
 
 	def weather_retrieved(self, request, data):
@@ -119,7 +138,28 @@ class CurrentWeather(BoxLayout):
 
 
 class WeatherApp(App):
-	pass
+	def build_config(self, config):
+		config.setdefaults('General', {'temp_type':'Metric'})
+
+	def build_settings(self, settings):
+		settings.add_json_panel("Weather Settings", self.config, data="""
+			[
+				{"type":"options",
+				 	"title":"Temperature System",
+				 	"section":"General",
+				 	"key":"temp_type",
+				 	"options": ["Metric", "Imperial"]
+				}
+			]""")
+
+	def on_config_change(self, config, section, key, value):
+		if config is self.config and key == "temp_type":
+			try:
+				self.root.children[0].update_weather()
+			except AttributeError:
+				pass
+
+	
 
 if __name__ == '__main__':
 	WeatherApp().run()
